83b8dcb72d089731ff5662c9170583d0d95c3792
Vaflandiy <136570152+Vaflandiy@users.noreply.github.com>
2024-12-18 13:34:28 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main()
 {
-    int matrix[100][100] = {0};
     int n;
+    int matrix[100][100] = {0};
 
-    cout << "Enter n and matrix" << endl;
+    cout << "Введите количество натуральных чисел и матрицу" << endl;
     cin >> n;
     
     fill_matrix(matrix, n);
@@ -15,7 +15,7 @@ int main()
     if (has_similar_rows) {
         change_mininmal_elements_in_columns(matrix, n, find_maximal_primal(matrix, n));
         cout << endl; // Создаю разделение, чтобы читать можно было нормально.
-        print_matrix(matrix, n);
+        show_matrix(matrix, n);
     }
 
     return 0;
