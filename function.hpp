bee98bd3a9ea81ae107e1b266787baa541549487
Vaflandiy <136570152+Vaflandiy@users.noreply.github.com>
2024-12-18 13:19:24 +0300

    Update function.hpp

--- function.hpp
+++ function.hpp
@@ -2,5 +2,5 @@ void fill_matrix(int matrix[100][100], int n);
 void print_matrix(int matrix[100][100], int n);
 int has_similar_rows(int matrix[100][100], int n);
 void change_mininmal_elements_in_columns(int matrix[100][100], int n, int value);
-bool is_primal(int n);
+bool is_target_a_primal_number(int n);
 int find_maximal_primal(int matrix[100][100], int n);
