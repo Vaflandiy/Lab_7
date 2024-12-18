d8f0aa18365ebd65c941049a43d7f91262bb7304
Vaflandiy <136570152+Vaflandiy@users.noreply.github.com>
2024-12-18 13:34:35 +0300

    Update function.hpp

--- function.hpp
+++ function.hpp
@@ -1,6 +1,6 @@
-void fill_matrix(int matrix[100][100], int n);
-void print_matrix(int matrix[100][100], int n);
-int has_similar_rows(int matrix[100][100], int n);
-void change_mininmal_elements_in_columns(int matrix[100][100], int n, int value);
-bool is_target_a_primal_number(int n);
-int find_maximal_primal(int matrix[100][100], int n);
+void show_matrix(int matrix[100][100], int n); // Распечатыываю Матрицу
+void fill_matrix_with_numbers(int matrix[100][100], int n); // Заполняю Матрицу.
+int has_similar_rows(int matrix[100][100], int n); // Проверяю на наличие одинаковых рядов
+bool is_target_a_primal_number(int n); // Цель изучения - простое число? 
+void change_mininmal_elements_in_columns(int matrix[100][100], int n, int value); // Изменяю элементы в колонне на нужные
+int find_maximal_primal(int matrix[100][100], int n); // Найти максимальное простое число.
